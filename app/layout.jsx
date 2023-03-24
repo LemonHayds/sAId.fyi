c181616eacaf2b81112f29dad4715f422a30ba25
Dev-Betterverse <117495872+LemonHayds@users.noreply.github.com>
2023-03-24 12:31:39 +0000

    font variable

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,13 +3,18 @@ import Navbar from "./auth/Navbar";
 import QueryWrapper from "./auth/QueryWrapper";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en">
       <head />
-      <body className="bg-slate-50 dark:bg-slate-900 min-h-screen text-slate-900 antialiased">
+      <body
+        className={`bg-slate-50 dark:bg-slate-900 min-h-screen text-slate-900 antialiased ${inter.variable}`}
+      >
         <QueryWrapper>
           <div className="flex flex-col min-h-screen">
             <Navbar />
